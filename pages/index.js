ff8979845cb1955844728ec3d33f1231e9dbdd42
MichelsSSs <michelhs@protonmail.com>
2022-06-21 17:20:56 -0300

    home1

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,8 @@
 import {useState} from 'react';
 
 function Home(){
-return <div><h1>Hone</h1><Contador/></div>
+return <div><h1>EM DESENVOLVIMENTO......................</h1></div>
 
-}
-
-function Contador(){
-    const [contador,setContador]=useState(0);
-
-function adicionarContador(){
-
-    setContador(contador+1);
-}
-function deletarContador(){
-
-    setContador(contador==0?contador:contador-1);
-}
-
-    return(
-<div>
-<div>{contador}</div>
-<button onClick={adicionarContador}>adicionar</button>
-<button onClick={deletarContador}>deletar</button>
-</div>
-    )
 }
 
 export default Home
